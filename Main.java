5c8b8b556bc66ffde8b467488a3ce5873d4e207d
kawamura <ta.kawamura.sys22@morijyobi.ac.jp>
2022-11-08 10:30:56 +0900

    Mainクラスに2人目追加

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main{
 	public static void main(String[] args){
-		double height = 1.78;
-		double weight = 80;
-		double BMI = weight/(height*height);
+		double[] height = new double{1.78,1.75};
+		double[] weight = new double{80,70};
+		for(int i=0; i<2;i++){
+		double BMI = weight[i]/(height[i]*height[i]);
 		String bmi = String.format("%.2f",BMI);
 		System.out.println(bmi);
 	}
